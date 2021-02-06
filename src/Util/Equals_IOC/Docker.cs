81b6ff3c95f4ba40698c3e3b054ab4e84d018ce7
Elir Ribeiro <elirweb@gmail.com>
2021-02-06 01:59:33 -0300

    add project

---
+++ src/Util/Equals_IOC/Docker.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Equals_IOC
+{
+    public class Docker
+    {
+        public static void Register(IServiceCollection service)
+        {
+            service.AddScoped<Equals_Application.Interfaces.IAcquirer, Equals_Application.App.AppAcquirer>();
+
+            service.AddScoped(typeof(Equals_Domain.Common.IRepository<>), typeof(Equals_Infra.Common.RepositoryBase<>));
+
+            service.AddScoped<Equals_Domain.Interfaces.IAcquirer, Equals_Infra.Repository.RepositoryAcquirer>();
+
+            service.AddScoped<Equals_Infra.Context.EfCore>();
+
+            service.AddScoped<Equals_Infra.Uow.IUoW, Equals_Infra.Uow.UnitOfWork>();
+        }
+
+    }
+}
