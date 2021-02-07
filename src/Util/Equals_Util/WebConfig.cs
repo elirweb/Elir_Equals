1e0b18d8116cb33f02c323056195a41f86b51aa2
Elir Ribeiro <elirweb@gmail.com>
2021-02-07 18:40:34 -0300

    teste

--- src/Util/Equals_Util/WebConfig.cs
+++ src/Util/Equals_Util/WebConfig.cs
@@ -7,7 +7,7 @@ namespace Equals_Util
     public static class WebConfig
     {
         public static string GetStrinConnection =>
-        @"Data Source=LAPTOP-PQH9GV4C;Initial Catalog=Equals_Elir;User ID=sa;password=elirweb";
+        @"Data Source=LAPTOP-PQH9GV4C;Initial Catalog=Equals_Elir;User ID={usuario_db};password={senha_db}";
 
         public static string naoprocessados => @"C:\naoprocessados\";
         public static string processados => @"C:\processados\";
